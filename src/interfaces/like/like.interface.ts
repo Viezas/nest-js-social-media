1669e1dce57fbdba053e3ab6cc198c1c89507832
Ziyi WANG <ziyiwang1027@gmail.com>
2023-04-24 12:03:01 +0200

    Setting interface like

--- src/interfaces/like/like.interface.ts
+++ src/interfaces/like/like.interface.ts
@@ -1,1 +1,8 @@
-export interface Like {}
+export interface Like {
+    id: number;
+    user_id: number;
+    likeable_type: string;
+    likeable_id: number;
+    created_at: Date;
+    updated_at: Date;
+}
